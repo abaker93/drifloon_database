52807f681d0988fb70f5974034a3ae3d30d95ae9
Anna Baker <62150513+abaker93@users.noreply.github.com>
2022-06-14 17:11:49 -0400

    basic info pulling from API. pulls in gen8 data if national dex

--- client/src/routes/Pokedex.js
+++ client/src/routes/Pokedex.js
@@ -1,42 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Chip from '@mui/material/Chip';
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
+import { GetDex } from '../utils/getDex';
+import { ChooseDex } from '../utils/utils';
 
 const Pokedex = () => {
 	const params = useParams();
-	// console.log(params.game)
 
-	let dex = 'national';
-
-	switch(params.game) {
-		case 'national':
-			dex = 'national';
-			break;
-		case 'red-blue':
-			dex = 'gen1'
-			break;
-		case 'yellow':
-			dex = 'gen1'
-			break;
-		default:
-			dex = 'national'
+	const formatDexNum = num => {
+		num = String(num);
+		while (num.length < 3) num = '0' + num;
+		return num;
 	}
 
-	const [data, setData] = useState([])
+	const totalStats = (hp, att, def, spatt, spdef, spd) => {
+		return(hp + att + def + spatt + spdef + spd)
+	}
 
-	useEffect(() => {
-		const getData = async () => {
-			const fetchedData = await fetch(`/api/${dex}`, { method: 'GET' })
-			const read = await fetchedData.json()
-			setData(read)
-		}
-		getData()
-	}, [dex])
+	const dex = ChooseDex(params.game).dex;
+	const title = ChooseDex(params.game).title;
+
+	const getDex = GetDex(dex)
+	const latestDex = GetDex('gen8')
 
 	return (
 		<>
@@ -44,13 +35,57 @@ const Pokedex = () => {
 			
 			<Container maxWidth="xl" sx={{ mt: 10 }}>
 				<Box>
-					<h1>Pokedex</h1>
-					{data.length
-						? data.map((poke, i) => (
-							<Box key={i}>
-								<img style={{ width: '100px' }} src={poke.artwork} alt={poke.name} />
-							</Box>
-						)) : null}
+					<h1>{title} Pokedex</h1>
+					{getDex.map((p) => (
+						<Box key={p.id}>
+							<img style={{ width: '100px' }} src={p.artwork} alt={p.name} />
+							<p><span className="no">No.</span> {formatDexNum(p.national)}</p>
+							<h2>{p.name}</h2>
+							{dex==='national'
+								? latestDex
+									.filter(pName => pName.name === p.name)
+									.map(poke => (
+										<div key={poke.id}>
+											<div>
+												<Chip label={poke.type1} size="small" />
+												{poke.type2 ? <Chip label={poke.type2} size="small" /> : null}
+											</div>
+											<div>
+												<div>
+													<p>HP</p>
+													<p>{poke.hp}</p>
+												</div>
+												<div>
+													<p>Att</p>
+													<p>{poke.att}</p>
+												</div>
+												<div>
+													<p>Def</p>
+													<p>{poke.def}</p>
+												</div>
+												<div>
+													<p>Sp.A</p>
+													<p>{poke.spAtt}</p>
+												</div>
+												<div>
+													<p>Sp.D</p>
+													<p>{poke.spDef}</p>
+												</div>
+												<div>
+													<p>Spd</p>
+													<p>{poke.spd}</p>
+												</div>
+												<div>
+													<p>Total</p>
+													<p>{totalStats(poke.hp, poke.att, poke.def, poke.spAtt, poke.spDef, poke.spd)}</p>
+												</div>
+											</div>
+										</div>
+									))
+								: 'not national'
+							}
+						</Box>
+					))}
 				</Box>
 			</Container>
 
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// GoogleAPI Tutorial: https://betterprogramming.pub/make-a-team-roster-with-node-react-and-google-sheets-305b9f9ebbb1
+// Redis Tutorial: https://www.sitepoint.com/using-redis-node-js/
+
 const { TablesServiceClient } = require('@google/area120-tables').v1alpha1;
 const express = require('express');
 const redis = require('redis');
@@ -29,11 +32,12 @@ async function callListRows(key) {
 		const ndI = tablesClient.listRowsAsync({ parent: p.NATIONAL_DEX_TABLE })
 		for await (const pokemon of ndI) {
 			nd.push({
-				name: pokemon.values.name ? pokemon.values.name.stringValue : n,
-				national: pokemon.values.national ? pokemon.values.national.numberValue : n,
-				type1: pokemon.values.type1 ? pokemon.values.type1.stringValue : n,
-				type2: pokemon.values.type2 ? pokemon.values.type2.stringValue : n,
-				artwork: pokemon.values.artwork ? pokemon.values.artwork.stringValue : n,
+				name:			pokemon.values.name ? pokemon.values.name.stringValue : n,
+				national:	pokemon.values.national ? pokemon.values.national.numberValue : n,
+				type1:		pokemon.values.type1 ? pokemon.values.type1.stringValue : n,
+				type2:		pokemon.values.type2 ? pokemon.values.type2.stringValue : n,
+				artwork:	pokemon.values.artwork ? pokemon.values.artwork.stringValue : n,
+				updateTime: pokemon.values.updateTime ?	pokemon.values.updateTime.structValue.fields.seconds.numberValue : n,
 				id: pokemon.values.id ? pokemon.values.id.stringValue : n,
 			})
 		}
@@ -43,17 +47,18 @@ async function callListRows(key) {
 		const gen1I = tablesClient.listRowsAsync({ parent: p.GEN_1_TABLE })
 		for await (const pokemon of gen1I) {
 			gen1.push({
-				name:					pokemon.values.name 				?	pokemon.values.name.stringValue 				: n,
-				national: 		pokemon.values.national 		? pokemon.values.national.numberValue 		: n,
-				kanto:				pokemon.values.kantoDex 		? pokemon.values.kantoDex.numberValue 		: n,
-				category: 		pokemon.values.category 		? pokemon.values.category.stringValue 		: n,
-				type1:				pokemon.values.type1 				? pokemon.values.type1.stringValue 				: n,
-				type2:				pokemon.values.type2 				? pokemon.values.type2.stringValue 				: n,
+				name:			pokemon.values.name 		?	pokemon.values.name.stringValue 		: n,
+				category:	pokemon.values.category ? pokemon.values.category.stringValue : n,
+				type1:		pokemon.values.type1 		? pokemon.values.type1.stringValue 		: n,
+				type2:		pokemon.values.type2 		? pokemon.values.type2.stringValue 		: n,
 				
-				redText:			pokemon.values.redText 			? pokemon.values.redText.stringValue 			: n,
-				greenText:		pokemon.values.greenText		? pokemon.values.greenText.stringValue 		: n,
-				blueText:			pokemon.values.blueText 		? pokemon.values.blueText.stringValue 		: n,
-				yellowText:		pokemon.values.yellowText		? pokemon.values.yellowText.stringValue		: n,
+				national: pokemon.values.national	? pokemon.values.national.numberValue : n,
+				kanto:		pokemon.values.kantoDex	? pokemon.values.kantoDex.numberValue : n,
+				
+				redText:		pokemon.values.redText 		? pokemon.values.redText.stringValue 		: n,
+				greenText:	pokemon.values.greenText	? pokemon.values.greenText.stringValue 	: n,
+				blueText:		pokemon.values.blueText 	? pokemon.values.blueText.stringValue 	: n,
+				yellowText:	pokemon.values.yellowText	? pokemon.values.yellowText.stringValue	: n,
 				
 				height:				pokemon.values.height		 		? pokemon.values.height.numberValue 			: n,
 				weight:				pokemon.values.weight				? pokemon.values.weight.numberValue 			: n,
@@ -68,31 +73,31 @@ async function callListRows(key) {
 				german:				pokemon.values.german				? pokemon.values.german.stringValue				: n,
 				korean:				pokemon.values.korean				? pokemon.values.korean.stringValue				: n,
 				
-				hp:						pokemon.values.hp						? pokemon.values.hp.numberValue						: n,
-				hp50Min:			pokemon.values.hp50Min			? pokemon.values.hp50Min.numberValue			: n,
-				hp50Max:			pokemon.values.hp50Max			? pokemon.values.hp50Max.numberValue			: n,
-				hp100Min:			pokemon.values.hp100Min			? pokemon.values.hp100Min.numberValue			: n,
-				hp100Max:			pokemon.values.hp100Max			? pokemon.values.hp100Max.numberValue			: n,
-				att:					pokemon.values.att					? pokemon.values.att.numberValue					: n,
-				att50Min:			pokemon.values.att50Min			? pokemon.values.att50Min.numberValue			: n,
-				att50Max:			pokemon.values.att50Max			? pokemon.values.att50Max.numberValue			: n,
-				att100Min:		pokemon.values.att100Min		? pokemon.values.att100Min.numberValue		: n,
-				att100Max:		pokemon.values.att100Max		? pokemon.values.att100Max.numberValue		: n,
-				def:					pokemon.values.def					? pokemon.values.def.numberValue					: n,
-				def50Min:			pokemon.values.def50Min			? pokemon.values.def50Min.numberValue			: n,
-				def50Max:			pokemon.values.def50Max			? pokemon.values.def50Max.numberValue			: n,
-				def100Min:		pokemon.values.def100Min		? pokemon.values.def100Min.numberValue		: n,
-				def100Max:		pokemon.values.def100Max		? pokemon.values.def100Max.numberValue		: n,
-				sp:						pokemon.values.sp						? pokemon.values.sp.numberValue						: n,
-				sp50Min:			pokemon.values.sp50Min			? pokemon.values.sp50Min.numberValue			: n,
-				sp50Max:			pokemon.values.sp50Max			? pokemon.values.sp50Max.numberValue			: n,
-				sp100Min:			pokemon.values.sp100Min			? pokemon.values.sp100Min.numberValue			: n,
-				sp100Max:			pokemon.values.sp100Max			? pokemon.values.sp100Max.numberValue			: n,
-				spd:					pokemon.values.spd					? pokemon.values.spd.numberValue					: n,
-				spd50Min:			pokemon.values.spd50Min			? pokemon.values.spd50Min.numberValue			: n,
-				spd50Max:			pokemon.values.spd50Max			? pokemon.values.spd50Max.numberValue			: n,
-				spd100Min:		pokemon.values.spd100Min		? pokemon.values.spd100Min.numberValue		: n,
-				spd100Max:		pokemon.values.spd100Max		? pokemon.values.spd100Max.numberValue		: n,
+				hp:					pokemon.values.hp					? pokemon.values.hp.numberValue					: n,
+				hp50Min:		pokemon.values.hp50Min		? pokemon.values.hp50Min.numberValue		: n,
+				hp50Max:		pokemon.values.hp50Max		? pokemon.values.hp50Max.numberValue		: n,
+				hp100Min:		pokemon.values.hp100Min		? pokemon.values.hp100Min.numberValue		: n,
+				hp100Max:		pokemon.values.hp100Max		? pokemon.values.hp100Max.numberValue		: n,
+				att:				pokemon.values.att				? pokemon.values.att.numberValue				: n,
+				att50Min:		pokemon.values.att50Min		? pokemon.values.att50Min.numberValue		: n,
+				att50Max:		pokemon.values.att50Max		? pokemon.values.att50Max.numberValue		: n,
+				att100Min:	pokemon.values.att100Min	? pokemon.values.att100Min.numberValue	: n,
+				att100Max:	pokemon.values.att100Max	? pokemon.values.att100Max.numberValue	: n,
+				def:				pokemon.values.def				? pokemon.values.def.numberValue				: n,
+				def50Min:		pokemon.values.def50Min		? pokemon.values.def50Min.numberValue		: n,
+				def50Max:		pokemon.values.def50Max		? pokemon.values.def50Max.numberValue		: n,
+				def100Min:	pokemon.values.def100Min	? pokemon.values.def100Min.numberValue	: n,
+				def100Max:	pokemon.values.def100Max	? pokemon.values.def100Max.numberValue	: n,
+				sp:					pokemon.values.sp					? pokemon.values.sp.numberValue					: n,
+				sp50Min:		pokemon.values.sp50Min		? pokemon.values.sp50Min.numberValue		: n,
+				sp50Max:		pokemon.values.sp50Max		? pokemon.values.sp50Max.numberValue		: n,
+				sp100Min:		pokemon.values.sp100Min		? pokemon.values.sp100Min.numberValue		: n,
+				sp100Max:		pokemon.values.sp100Max		? pokemon.values.sp100Max.numberValue		: n,
+				spd:				pokemon.values.spd				? pokemon.values.spd.numberValue				: n,
+				spd50Min:		pokemon.values.spd50Min		? pokemon.values.spd50Min.numberValue		: n,
+				spd50Max:		pokemon.values.spd50Max		? pokemon.values.spd50Max.numberValue		: n,
+				spd100Min:	pokemon.values.spd100Min	? pokemon.values.spd100Min.numberValue	: n,
+				spd100Max:	pokemon.values.spd100Max	? pokemon.values.spd100Max.numberValue	: n,
 				
 				spriteNorRB:	pokemon.values.spriteNorRB	? pokemon.values.spriteNorRB.stringValue	: n,
 				spriteNorY:		pokemon.values.spriteNorY		? pokemon.values.spriteNorY.stringValue		: n,
@@ -104,52 +109,124 @@ async function callListRows(key) {
 			})
 		}
 		return gen1;
+	} else if (key === 'gen8') {
+			const gen8 = [];
+			const gen8I = tablesClient.listRowsAsync({ parent: p.GEN_8_TABLE })
+			for await (const pokemon of gen8I) {
+				gen8.push({
+					// data: pokemon.values,
+					name:			pokemon.values.name 		?	pokemon.values.name.stringValue 		: n,
+					category:	pokemon.values.category	? pokemon.values.category.stringValue	: n,
+					type1:		pokemon.values.type1 		? pokemon.values.type1.stringValue 		: n,
+					type2:		pokemon.values.type2 		? pokemon.values.type2.stringValue 		: n,
+					
+					national: pokemon.values.national ? pokemon.values.national.numberValue : n,
+					galar:		pokemon.values.galar 		? pokemon.values.galar.numberValue		: n,
+					galarIOA:	pokemon.values.galarIOA	? pokemon.values.galarIOA.numberValue	: n,
+					galarCT:	pokemon.values.galarCT 	? pokemon.values.galarCT.numberValue 	: n,
+					sinnoh:		pokemon.values.sinnoh 	? pokemon.values.sinnoh.numberValue 	: n,
+					hisui:		pokemon.values.hisui 		? pokemon.values.hisui.numberValue 		: n,
+					
+					swordText:						pokemon.values.swordText 						? pokemon.values.swordText.stringValue 						: n,
+					shieldText:						pokemon.values.shieldText						? pokemon.values.shieldText.stringValue 					: n,
+					brilliantDiamondText:	pokemon.values.brilliantDiamondText	? pokemon.values.brilliantDiamondText.stringValue	: n,
+					shiningPearlText:			pokemon.values.shiningPearlText			? pokemon.values.shiningPearlText.stringValue			: n,
+					legendsArceusText:		pokemon.values.legendsArceusText		? pokemon.values.legendsArceusText.stringValue		: n,
+					
+					height:			pokemon.values.height						? pokemon.values.height.numberValue						: n,
+					weight:			pokemon.values.weight						? pokemon.values.weight.numberValue						: n,
+								// QUESTIONABLE ???????????
+					abilities: {
+						abilities:	pokemon.values.abilities			?	pokemon.values.abilities.listValue.values.map(ability => {return(ability.stringValue)}) : n,
+						urls:				pokemon.values.gen8Abilities	? pokemon.values.gen8Abilities.listValue.values.map(url => {return(url.stringValue)}) 		: n,
+					},
+					hiddenAbility: {
+						ability:	pokemon.values.hiddenAbility				? pokemon.values.hiddenAbility.stringValue				: n,
+						url:			pokemon.values.gen8HiddenAbilities	? pokemon.values.gen8HiddenAbilities.stringValue	: n,
+					},
+					MGendRatio:			pokemon.values.MGendRatio			? pokemon.values.MGendRatio.numberValue				: n,
+					FGendRatio:			pokemon.values.FGendRatio			? pokemon.values.FGendRatio.numberValue				: n,
+					evYield:				pokemon.values.evYield				?	pokemon.values.evYield.listValue.values.map(ev => {return(ev.stringValue)})	: n,
+					catchRate:			pokemon.values.catchRate			? pokemon.values.catchRate.numberValue 				: n,
+					baseFriendship:	pokemon.values.baseFriendship	? pokemon.values.baseFriendship.numberValue 	: n,
+					baseXP:					pokemon.values.baseXP					? pokemon.values.baseXP.numberValue 					: n,
+					growthRate:			pokemon.values.growthRate			? pokemon.values.growthRate.stringValue				: n,
+					eggGroups: {
+						eggGroups:	pokemon.values.eggGroups			?	pokemon.values.eggGroups.listValue.values.map(group => {return(group.stringValue)}) : n,
+						urls:				pokemon.values.gen8EggGroups	? pokemon.values.gen8EggGroups.listValue.values.map(url => {return(url.stringValue)})	: n,
+					},
+					eggCycles:			pokemon.values.eggCycles			? pokemon.values.eggCycles.numberValue 				: n,
+					
+					japanese:			pokemon.values.japanese			? pokemon.values.japanese.stringValue			: n,
+					japaneseKata:	pokemon.values.japaneseKata	? pokemon.values.japaneseKata.stringValue	: n,
+					french:				pokemon.values.french				? pokemon.values.french.stringValue				: n,
+					german:				pokemon.values.german				? pokemon.values.german.stringValue				: n,
+					korean:				pokemon.values.korean				? pokemon.values.korean.stringValue				: n,
+					
+					hp:						pokemon.values.hp						? pokemon.values.hp.numberValue						: n,
+					hp50Min:			pokemon.values.hp50Min			? pokemon.values.hp50Min.numberValue			: n,
+					hp50Max:			pokemon.values.hp50Max			? pokemon.values.hp50Max.numberValue			: n,
+					hp100Min:			pokemon.values.hp100Min			? pokemon.values.hp100Min.numberValue			: n,
+					hp100Max:			pokemon.values.hp100Max			? pokemon.values.hp100Max.numberValue			: n,
+					att:					pokemon.values.att					? pokemon.values.att.numberValue					: n,
+					att50Min:			pokemon.values.att50Min			? pokemon.values.att50Min.numberValue			: n,
+					att50Max:			pokemon.values.att50Max			? pokemon.values.att50Max.numberValue			: n,
+					att100Min:		pokemon.values.att100Min		? pokemon.values.att100Min.numberValue		: n,
+					att100Max:		pokemon.values.att100Max		? pokemon.values.att100Max.numberValue		: n,
+					def:					pokemon.values.def					? pokemon.values.def.numberValue					: n,
+					def50Min:			pokemon.values.def50Min			? pokemon.values.def50Min.numberValue			: n,
+					def50Max:			pokemon.values.def50Max			? pokemon.values.def50Max.numberValue			: n,
+					def100Min:		pokemon.values.def100Min		? pokemon.values.def100Min.numberValue		: n,
+					def100Max:		pokemon.values.def100Max		? pokemon.values.def100Max.numberValue		: n,
+					spAtt:				pokemon.values.spAtt				? pokemon.values.spAtt.numberValue				: n,
+					spAtt50Min:		pokemon.values.spAtt50Min		? pokemon.values.spAtt50Min.numberValue		: n,
+					spAtt50Max:		pokemon.values.spAtt50Max		? pokemon.values.spAtt50Max.numberValue		: n,
+					spAtt100Min:	pokemon.values.spAtt100Min	? pokemon.values.spAtt100Min.numberValue	: n,
+					spAtt100Max:	pokemon.values.spAtt100Max	? pokemon.values.spAtt100Max.numberValue	: n,
+					spDef:				pokemon.values.spDef				? pokemon.values.spDef.numberValue				: n,
+					spDef50Min:		pokemon.values.spDef50Min		? pokemon.values.spDef50Min.numberValue		: n,
+					spDef50Max:		pokemon.values.spDef50Max		? pokemon.values.spDef50Max.numberValue		: n,
+					spDef100Min:	pokemon.values.spDef100Min	? pokemon.values.spDef100Min.numberValue	: n,
+					spDef100Max:	pokemon.values.spDef100Max	? pokemon.values.spDef100Max.numberValue	: n,
+					spd:					pokemon.values.spd					? pokemon.values.spd.numberValue					: n,
+					spd50Min:			pokemon.values.spd50Min			? pokemon.values.spd50Min.numberValue			: n,
+					spd50Max:			pokemon.values.spd50Max			? pokemon.values.spd50Max.numberValue			: n,
+					spd100Min:		pokemon.values.spd100Min		? pokemon.values.spd100Min.numberValue		: n,
+					spd100Max:		pokemon.values.spd100Max		? pokemon.values.spd100Max.numberValue		: n,
+					
+					// spriteNorRB:	pokemon.values.spriteNorRB	? pokemon.values.spriteNorRB.stringValue	: n,
+					// spriteNorY:		pokemon.values.spriteNorY		? pokemon.values.spriteNorY.stringValue		: n,
+					// spriteBackRB:	pokemon.values.spriteBackRB	? pokemon.values.spriteBackRB.stringValue	: n,
+					// spriteBackY:	pokemon.values.spriteBackY	? pokemon.values.spriteBackY.stringValue	: n,
+	
+					updateTime: pokemon.values.updateTime ?	pokemon.values.updateTime.structValue.fields.seconds.numberValue : n,
+					id: pokemon.values.id ? pokemon.values.id.stringValue : n,
+				})
+			}
+			return gen8;
 	} else {
 		return n;
 	}
 }
 
-app.get('/api/national', async (req, res, next) => {
+app.get('/api/:gen', async(req, res, next) => {
 	try {
 		let results = null;
-
-		const key = 'national'
+		const key = req.params.gen
 
 		const value = await client.get(key);
 		if (value) {
 			results = JSON.parse(value);
-			console.log('from CACHE data [NATIONAL]')
+			console.log(`CACHE data from ${key.toUpperCase()}`)
 		} else {
-			console.log('from SOURCE data [NATIONAL]')
+			console.log(`SOURCE data from ${key.toUpperCase()}`)
 			results = await callListRows(key);
-			client.set(key, JSON.stringify(results), {EX: 60});
+			client.set(key, JSON.stringify(results), {EX: (60 * 3)});
 		}
 
 		res.json(results)
 	} catch (err) {
 		next(err)
-	}
-})
-
-app.get('/api/gen1', async (req, res, next) => {
-	try {
-		let results = null;
-
-		const key = 'gen1'
-
-		const value = await client.get(key);
-		if (value) {
-			results = JSON.parse(value);
-			console.log('from CACHE data [GEN 1]')
-		} else {
-			console.log('from SOURCE data [GEN 1]')
-			results = await callListRows(key)
-			client.set(key, JSON.stringify(results), {EX: 60});
-		}
-
-		res.json(results)
-	} catch (err) {
-		next (err)
 	}
 })
 
